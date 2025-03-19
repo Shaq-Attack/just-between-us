f83689574cafc653392e146894032905080114b8
Shaq-Attack <s.niekerk911@gmail.com>
2025-03-19 21:56:44 +0200

    Implement comments feature in MessageBoard with UI updates and state management

--- src/pages/read.tsx
+++ src/pages/read.tsx
@@ -1,9 +1,9 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement, use, useEffect, useState } from 'react';
 import { GridLayout, GridLayoutItem } from '@progress/kendo-react-layout';
 import '@progress/kendo-theme-default/dist/all.css';
 import { Card, CardHeader, CardBody, CardTitle, CardActions } from '@progress/kendo-react-layout';
 import { Button } from '@progress/kendo-react-buttons';
-import { Message, ReadAll, ReadByTitle } from '../RecordKeeper';
+import { Message, ReadAll, ReadByTitle, Update } from '../RecordKeeper';
 import { Firestore } from 'firebase/firestore';
 import { Typography } from '@progress/kendo-react-common';
 import { Window } from '@progress/kendo-react-dialogs';
@@ -18,24 +18,23 @@ const Read = (props: { isLightMode: boolean; db: Firestore }): ReactElement => {
     const [data, setData] = useState<Message[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [visibleWindow, setVisibleWindow] = useState<boolean>(false);
-    const [title, setTitle] = useState<string>('');
+    const [commentWindow, setCommentWindow] = useState<boolean>(false);
+    const [post, setPost] = useState<Message>();
+    const [comment, setComment] = useState<string>('');
     const [searchTitle, setSearchTitle] = useState<string>('');
-    const [body, setBody] = useState<string>('');
     const [error, setError] = useState<boolean>(false);
 
     useEffect(() => {
-        setLoading(true);
-        ReadAll(db)
-            .then((result) => setData(randomise(result)))
-            .finally(() => setLoading(false));
-    }, [db]);
+        if (!commentWindow) {
+            setLoading(true);
+            ReadAll(db)
+                .then((result) => setData(randomise(result)))
+                .finally(() => setLoading(false));
+        }
+    }, [db, commentWindow]);
 
     const randomise = (array: Message[]): Message[] => {
         return array.sort(() => Math.random() - 0.5);
-    };
-
-    const toggleWindow = () => {
-        setVisibleWindow(!visibleWindow);
     };
 
     const truncate = (text: string, size: number): string => {
@@ -46,20 +45,24 @@ const Read = (props: { isLightMode: boolean; db: Firestore }): ReactElement => {
     };
 
     const showMore = (item: Message) => {
-        setTitle(item.title);
-        setBody(item.body);
-        toggleWindow();
+        setPost(item);
+        setVisibleWindow(true);
+    };
+
+    const enterComment = (item: Message) => {
+        setPost(item);
+        setCommentWindow(true);
     };
 
     return (
         <div className="WebPage">
-            {loading && !visibleWindow && (
+            {loading && !visibleWindow && !commentWindow && (
                 <div className="Loader">
                     <Loader type="infinite-spinner" themeColor={GetThemeColourV2(isLightMode)} />
                     <Typography.p>Retrieving Posts...</Typography.p>
                 </div>
             )}
-            {!loading && !visibleWindow && (
+            {!loading && (
                 <div
                     style={{
                         display: 'flex',
@@ -101,12 +104,11 @@ const Read = (props: { isLightMode: boolean; db: Firestore }): ReactElement => {
                                 >
                                     {data.map((item, index) => (
                                         <GridLayoutItem
-                                            className="grid-box"
                                             key={index}
                                             row={Math.floor(index / 3) + 1}
                                             col={(index % 3) + 1}
                                         >
-                                            <Card className="Card" style={{ width: 400, margin: 'auto' }}>
+                                            <Card className="Card" style={{ height: 170, width: 400, margin: 'auto' }}>
                                                 <CardHeader>
                                                     <CardTitle>{truncate(item.title, 50)}</CardTitle>
                                                 </CardHeader>
@@ -121,6 +123,14 @@ const Read = (props: { isLightMode: boolean; db: Firestore }): ReactElement => {
                                                         onClick={() => showMore(item)}
                                                     >
                                                         Read more
+                                                    </Button>
+                                                    <Button
+                                                        fillMode="flat"
+                                                        themeColor={GetThemeColour(!isLightMode)}
+                                                        type="button"
+                                                        onClick={() => enterComment(item)}
+                                                    >
+                                                        View Comments
                                                     </Button>
                                                 </CardActions>
                                             </Card>
@@ -156,13 +166,58 @@ const Read = (props: { isLightMode: boolean; db: Firestore }): ReactElement => {
                 <Window
                     className="Card"
                     themeColor={GetThemeColour(isLightMode)}
-                    title={title}
-                    onClose={toggleWindow}
+                    title={post?.title}
+                    onClose={() => setVisibleWindow(false)}
                     initialHeight={300}
                     initialWidth={500}
                     resizable={true}
                 >
-                    <Typography.p>{body}</Typography.p>
+                    <Typography.p>{post?.body}</Typography.p>
+                </Window>
+            )}
+            {commentWindow && (
+                <Window
+                    className="Card"
+                    themeColor={GetThemeColour(isLightMode)}
+                    title={`Comments on: ${post?.title}`}
+                    onClose={() => setCommentWindow(false)}
+                    initialHeight={300}
+                    initialWidth={500}
+                    resizable={true}
+                >
+                    {post?.comments.map((item, index) => (
+                        <div key={index}>
+                            <ul>
+                                <li>
+                                    <Typography.p>{item}</Typography.p>
+                                </li>
+                            </ul>
+                        </div>
+                    ))}
+                    <Typography.h3>Enter your comment:</Typography.h3>
+                    <TextBox
+                        className="TextInput"
+                        placeholder="Comment..."
+                        value={comment}
+                        onChange={(e) => setComment((e.target.value as string) ?? '')}
+                    />
+                    <Button
+                        style={{ marginTop: '1rem' }}
+                        fillMode="solid"
+                        themeColor={GetThemeColour(isLightMode)}
+                        type="button"
+                        onClick={() => {
+                            let newPost = post as Message;
+                            if (post) {
+                                newPost = { ...post, comments: [...post.comments, comment] };
+                                setPost({ ...post, comments: [...post.comments, comment] });
+                            }
+                            setComment('');
+                            Update(db, newPost?.title ?? '', newPost);
+                        }}
+                    >
+                        Add Comment
+                    </Button>
                 </Window>
             )}
         </div>
